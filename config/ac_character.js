806ee3878942f8f06ee95de60ffb3df54360b975
Snowbolt Games <snowboltgameshq@gmail.com>
2024-10-25 18:46:47 +0100

    modified:   config/ac_character.js

--- config/ac_character.js
+++ config/ac_character.js
@@ -1,8 +1,14 @@
 const Attributes = class {
     constructor(attribute_type, attribute_data) {
         if (attribute_type == "sleeve") {
+            this.strength = attribute_data["strength"]
+            this.perception = attribute_data["perception"]
             console.log("Attributes Type:", attribute_type, "Loaded");
         } else if (attribute_type == "dhf") {
+            this.empathy = attribute_data["empathy"]
+            this.willpower = attribute_data["willpower"]
+            this.acuity = attribute_data["acuity"]
+            this.intelligence = attribute_data["intelligence"]
             console.log("Attributes Type:", attribute_type, "Loaded");
         } else {
             console.log();
@@ -39,8 +45,8 @@ const Player = class {
     constructor(name, archetype) {
         this.name = name;
         this.archetype = new Archetype(archetype);
-        this.dhf = new DHF({});
-        this.sleeve = new Sleeve({});
+        this.dhf = new DHF({"empathy": 0, "willpower": 0, "acuity": 0, "intelligence": 0});
+        this.sleeve = new Sleeve({"strength": 0, "perception": 0});
         this.bank_account = new BankAccount(100, 0);
     }
 };
