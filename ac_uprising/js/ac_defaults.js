5c71dcf52f1efd5469ebec95b461d492500593e4
Snowbolt Games <snowboltgameshq@gmail.com>
2024-10-27 18:14:02 +0000

    modified:   ac_uprising/js/ac_defaults.js

--- ac_uprising/js/ac_defaults.js
+++ ac_uprising/js/ac_defaults.js
@@ -1,5 +1,13 @@
 const RelacementSleeve = class {
-    constructor() {
-        this.sleeve_type = ""
+    constructor(sleeve_value) {
+        if (sleeve_value == 0) {
+            this.sleeve_type = "natal";
+        } else if (sleeve_value == 1) {
+            this.sleeve_type = "synth_low";
+        } else if (sleeve_value == 2) {
+            this.sleeve_type = "synth_med";
+        } else if (sleeve_value == 3) {
+            this.sleeve_type = "synth_high";
+        };
     };
 };
