5bd571b2cb9cc32b53f0689d1e8c123288a96034
Snowbolt Games <snowboltgameshq@gmail.com>
2024-10-27 21:30:10 +0000

    modified:   ac_uprising/js/ac_character.js

--- ac_uprising/js/ac_character.js
+++ ac_uprising/js/ac_character.js
@@ -409,7 +409,7 @@ export function replace_sleeve(player, sleeve_value) {
     player.sleeve = new_sleeve;
 };
 
-export class Player {
+export default class Player {
     constructor(name, archetype) {
         this.name = name;
         this.archetype = new Archetype(null, archetype);
