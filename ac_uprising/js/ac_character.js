e5d191c58c8ce57ea611fa8a84057ac545b39ad8
Snowbolt Games <snowboltgameshq@gmail.com>
2024-10-27 17:03:33 +0000

    modified:   ac_uprising/js/ac_character.js

--- ac_uprising/js/ac_character.js
+++ ac_uprising/js/ac_character.js
@@ -380,7 +380,7 @@ const BankAccount = class {
     };
     get_balance = function() {
         if (this.balance < 1000) {
-            return ""+this.balance/1000+" (μCr)";
+            return ""+this.balance+" (μCr)";
         } else if (this.balance >= 1000) {
             return ""+this.balance/1000+" (Cr)";
         } else if (this.balance >= 1000000) {
@@ -392,6 +392,7 @@ const BankAccount = class {
     add_balance = function(amount, credit_type) {
         _uncr = new UNCredit(amount, credit_type);
         if (credit_type == "μCr") {
+            _cr = amount/1000;
             this.balance+=amount;
         } else if (credit_type == "Cr") {
             _cr = amount*1000;
