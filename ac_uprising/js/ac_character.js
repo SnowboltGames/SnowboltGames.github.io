3f9dc634cb47eb770eeb4cb26303f1f078eb4159
Snowbolt Games <snowboltgameshq@gmail.com>
2024-10-27 18:22:50 +0000

    modified:   ac_uprising/js/ac_character.js modified:   ac_uprising/js/ac_defaults.js modified:   altered_corbon_uprising.html

--- ac_uprising/js/ac_character.js
+++ ac_uprising/js/ac_character.js
@@ -411,6 +411,12 @@ const BankAccount = class {
     };
 };
 
+function replace_sleeve(player, sleeve_value) {
+    let new_sleeve_data = new ReplacementSleeve(sleeve_value);
+    let new_sleeve = Sleeve(new_sleeve_data.sleeve_type, new_sleeve_data.sleeve_data);
+    player.sleeve = new_sleeve;
+};
+
 const Player = class {
     constructor(name, archetype) {
         this.name = name;
--- ac_uprising/js/ac_defaults.js
+++ ac_uprising/js/ac_defaults.js
@@ -2,12 +2,80 @@ const RelacementSleeve = class {
     constructor(sleeve_value) {
         if (sleeve_value == 0) {
             this.sleeve_type = "natal";
+            this.sleeve_data = {
+                "strength": {
+                    "athletics": [1, "d8"], 
+                    "brawl": [1, "d8"], 
+                    "endurance": [1, "d8"], 
+                    "melee_combat": [1, "d8"], 
+                    "toughness": [1, "d8"]
+                }, 
+                "perception": {
+                    "detection": [1, "d8"], 
+                    "d_energy_weapons": [1, "d8"], 
+                    "firearms": [1, "d8"], 
+                    "search": [1, "d8"], 
+                    "stealth": [1, "d8"], 
+                    "throw": [1, "d8"]
+                }
+            };
         } else if (sleeve_value == 1) {
             this.sleeve_type = "synth_low";
+            this.sleeve_data = {
+                "strength": {
+                    "athletics": [1, "d8"], 
+                    "brawl": [1, "d8"], 
+                    "endurance": [1, "d8"], 
+                    "melee_combat": [1, "d8"], 
+                    "toughness": [1, "d8"]
+                }, 
+                "perception": {
+                    "detection": [1, "d8"], 
+                    "d_energy_weapons": [1, "d8"], 
+                    "firearms": [1, "d8"], 
+                    "search": [1, "d8"], 
+                    "stealth": [1, "d8"], 
+                    "throw": [1, "d8"]
+                }
+            };
         } else if (sleeve_value == 2) {
             this.sleeve_type = "synth_med";
+            this.sleeve_data = {
+                "strength": {
+                    "athletics": [1, "d8"], 
+                    "brawl": [1, "d8"], 
+                    "endurance": [1, "d8"], 
+                    "melee_combat": [1, "d8"], 
+                    "toughness": [1, "d8"]
+                }, 
+                "perception": {
+                    "detection": [1, "d8"], 
+                    "d_energy_weapons": [1, "d8"], 
+                    "firearms": [1, "d8"], 
+                    "search": [1, "d8"], 
+                    "stealth": [1, "d8"], 
+                    "throw": [1, "d8"]
+                }
+            };
         } else if (sleeve_value == 3) {
             this.sleeve_type = "synth_high";
+            this.sleeve_data = {
+                "strength": {
+                    "athletics": [1, "d8"], 
+                    "brawl": [1, "d8"], 
+                    "endurance": [1, "d8"], 
+                    "melee_combat": [1, "d8"], 
+                    "toughness": [1, "d8"]
+                }, 
+                "perception": {
+                    "detection": [1, "d8"], 
+                    "d_energy_weapons": [1, "d8"], 
+                    "firearms": [1, "d8"], 
+                    "search": [1, "d8"], 
+                    "stealth": [1, "d8"], 
+                    "throw": [1, "d8"]
+                }
+            };
         };
     };
 };
